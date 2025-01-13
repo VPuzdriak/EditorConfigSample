b812ed0eed589ff8f13a6723bf8e51d66370b8c0
Volodymyr Puzdriak <vpuzdriak@independer.nl>
2025-01-13 11:30:38 +0100

    Format solution files

--- EditorConfig.Api/ApplicationBuilderExtensions.cs
+++ EditorConfig.Api/ApplicationBuilderExtensions.cs
@@ -4,34 +4,29 @@ using EditorConfig.Api.Movies;
 
 namespace EditorConfig.Api;
 
-public static class ApplicationBuilderExtensions
-{
-    public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder)
-    {
-        var currentAssembly = Assembly.GetExecutingAssembly();
-        var endpointType = typeof(IEndpoint);
+public static class ApplicationBuilderExtensions {
+  public static IEndpointRouteBuilder MapEndpoints(this IEndpointRouteBuilder builder) {
+    var currentAssembly = Assembly.GetExecutingAssembly();
+    var endpointType = typeof(IEndpoint);
 
-        foreach (var type in currentAssembly.GetTypes())
-        {
-            if (endpointType.IsAssignableFrom(type) && !type.IsInterface)
-            {
-                var endpoint = (IEndpoint)Activator.CreateInstance(type)!;
-                endpoint.Map(builder);
-            }
-        }
-
-        return builder;
+    foreach (var type in currentAssembly.GetTypes()) {
+      if (endpointType.IsAssignableFrom(type) && !type.IsInterface) {
+        var endpoint = (IEndpoint)Activator.CreateInstance(type)!;
+        endpoint.Map(builder);
+      }
     }
 
-    public static async Task EnsureDatabaseCreatedAsync(this IApplicationBuilder builder)
-    {
-        ArgumentNullException.ThrowIfNull(builder);
-        
-        await using var scope = builder.ApplicationServices.CreateAsyncScope();
+    return builder;
+  }
 
-        var context = scope.ServiceProvider.GetRequiredService<MoviesDbContext>();
-        await context.Database.EnsureCreatedAsync();
+  public static async Task EnsureDatabaseCreatedAsync(this IApplicationBuilder builder) {
+    ArgumentNullException.ThrowIfNull(builder);
 
-        await context.SeedAsync(CancellationToken.None);
-    }
+    await using var scope = builder.ApplicationServices.CreateAsyncScope();
+
+    var context = scope.ServiceProvider.GetRequiredService<MoviesDbContext>();
+    await context.Database.EnsureCreatedAsync();
+
+    await context.SeedAsync(CancellationToken.None);
+  }
 }
